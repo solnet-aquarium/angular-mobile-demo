e4b5205e027ef7a5d4373c2b7f851c0573b2ecd7
Carson Bruce <carson.bruce@solnet.co.nz>
2015-04-03 18:27:00 +1300

    Main content list styling

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,12 +1,36 @@
 'use strict';
 
 angular.module('trusteesApp')
-  .controller('MainCtrl', function ($scope, $http, socket) {
+  .controller('MainCtrl', function ($scope, $http, socket, main, $log, $state, Auth, $timeout) {
 
 
 
+    main.getMainItems().then(function(items){
+      $log.debug('MainCtrl items', items);
+      $scope.items = items;
+    });
 
 
+    $scope.navigateTo = navigateTo;
+    $scope.logout = logout;
 
+
+    return;
+
+
+    /**
+     *
+     * @param state
+     */
+    function navigateTo(state){
+      $state.go(state);
+    }
+
+    function logout(){
+      Auth.logout();
+      $timeout(function(){
+        $state.go('login');
+      });
+    }
 
   });
