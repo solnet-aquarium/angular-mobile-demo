5e4e3098f8d14d505bd11d1d155c8598daeecd66
Carson Bruce <carson.bruce@solnet.co.nz>
2015-04-05 18:38:58 +1200

    workaround to ios flash. Displaying quickaccess panel with a delay

--- client/app/account/quickaccess/quickaccess.controller.js
+++ client/app/account/quickaccess/quickaccess.controller.js
@@ -1,131 +1,139 @@
 'use strict';
 
-angular.module('trusteesApp')
-  .controller('QuickaccessCtrl', function ($scope, Auth, $location, Logger, $state, $timeout, $rootScope, $window) {
+if (!angular.module('trusteesApp')
+    .controller('QuickaccessCtrl', function ($scope, Auth, $location, Logger, $state, $timeout, $rootScope, $window) {
 
 
-    var self = this;
+      var self = this;
 
-    var logger = Logger.getLogger($scope.controllerName);
+      var logger = Logger.getLogger($scope.controllerName);
 
-    $scope.email = undefined;
-    $scope.error = undefined;
-    $scope.success = undefined;
-    $scope.clearPin = _.debounce(clearPin, 60, true);
-    $scope.back = _.debounce(back, 50, true);
-    $scope.pin = '';
-
-    $scope.throttlePinDigit = _.debounce(addPinDigit, 30, true);
-
-    var supportsVibrate = 'vibrate' in navigator;
-
-    return init();
-
-
-    /**
-     *
-     */
-    function init(){
-      $scope.email = Auth.getQuickAccessUser();
-
-      if(!$scope.email){
-        $state.go('login');
-      }
-    }
-
-    /**
-     *
-     */
-    function clearPin(){
+      $scope.email = undefined;
+      $scope.error = undefined;
+      $scope.success = undefined;
+      $scope.active = undefined;
+      $scope.clearPin = _.debounce(clearPin, 60, true);
+      $scope.back = _.debounce(back, 50, true);
       $scope.pin = '';
 
-    }
+      $scope.throttlePinDigit = _.debounce(addPinDigit, 30, true);
 
-    function clearError(){
-      $scope.error = undefined;
-    }
+      var supportsVibrate = 'vibrate' in navigator;
 
-    function addError(error){
+      return init();
 
-      if(supportsVibrate){
-        navigator.vibrate(150);
-      }
 
-      $scope.error = error;
-      $timeout(function(){
-        if($scope.error){
-          clearError();
-          clearPin();
-        }
-      }, 500);
-    }
+      /**
+       *
+       */
+      function init() {
+        $scope.email = Auth.getQuickAccessUser();
 
-    /**
-     *
-     */
-    function back(){
-      logger.debug('quick access ctrl back');
+        //delay the displaying of the pin panel by an unnoticable amount
+        //    was seeing the view flash in on ios - not applying translate animation styling fast enough?
+        $timeout(function () {
+          $scope.active = true;
+        }, 150);
 
-      $window.history.back();
-    }
-
-    /**
-     *
-     */
-    function addPinDigit(digit){
-
-      logger.debug('adding pin digit');
-
-      if($scope.pin.length > 4){
-        if($scope.error){
-          clearError();
-          clearPin();
-        }
-        else {
-          return;
+        if (!$scope.email) {
+          $state.go('login');
         }
       }
 
-      if(supportsVibrate){
-        navigator.vibrate(50);
+      /**
+       *
+       */
+      function clearPin() {
+        $scope.pin = '';
+
       }
 
-      $scope.pin = $scope.pin.concat(digit);
-
-      if($scope.pin.length === 4){
-        login($scope.email, $scope.pin);
+      function clearError() {
+        $scope.error = undefined;
       }
 
-    }
+      function addError(error) {
+
+        if (supportsVibrate) {
+          navigator.vibrate(150);
+        }
+
+        $scope.error = error;
+        $timeout(function () {
+          if ($scope.error) {
+            clearError();
+            clearPin();
+          }
+        }, 500);
+      }
+
+      /**
+       *
+       */
+      function back() {
+        logger.debug('quick access ctrl back');
+
+        $window.history.back();
+      }
+
+      /**
+       *
+       */
+      function addPinDigit(digit) {
+
+        logger.debug('adding pin digit');
+
+        if ($scope.pin.length > 4) {
+          if ($scope.error) {
+            clearError();
+            clearPin();
+          }
+          else {
+            return;
+          }
+        }
+
+        if (supportsVibrate) {
+          navigator.vibrate(50);
+        }
+
+        $scope.pin = $scope.pin.concat(digit);
+
+        if ($scope.pin.length === 4) {
+          login($scope.email, $scope.pin);
+        }
+
+      }
 
 
-    /**
-     *
-     */
-    function login(email, pin){
+      /**
+       *
+       */
+      function login(email, pin) {
 
-      Auth.login({
-        email: email,
-        password: pin
-      })
-      .then( function() {
-        // Logged in, redirect to home
-        $scope.success = true;
-        $timeout(function(){
-          window.history.replaceState( {} , 'main', '/' );
-        });
+        Auth.login({
+          email: email,
+          password: pin
+        })
+          .then(function () {
+            // Logged in, redirect to home
+            $scope.success = true;
+            $timeout(function () {
+              window.history.replaceState({}, 'main', '/');
+            });
 
-          //TODO duplicate???
-        $rootScope.temporaryLoginUser = undefined;
-        Auth.setQuickAccessUser(undefined);
-      })
-      .catch( function(err) {
-          logger.debug('error logging in');
-          addError(err.message);
-      });
+            //TODO duplicate???
+            $rootScope.temporaryLoginUser = undefined;
+            Auth.setQuickAccessUser(undefined);
+          })
+          .catch(function (err) {
+            logger.debug('error logging in');
+            addError(err.message);
+          });
 
-    }
+      }
 
 
+    })) {
 
-  });
+}
