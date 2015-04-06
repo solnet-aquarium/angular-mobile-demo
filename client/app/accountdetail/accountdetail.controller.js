fd99e2d6fd760cd9c21f5520185baae30046cfe6
Carson Bruce <carson.bruce@solnet.co.nz>
2015-04-06 13:28:15 +1200

    dummy transaction dialogs and quickaccess pin animations

--- client/app/accountdetail/accountdetail.controller.js
+++ client/app/accountdetail/accountdetail.controller.js
@@ -1,13 +1,37 @@
 'use strict';
 
 angular.module('trusteesApp')
-  .controller('AccountdetailCtrl', function ($scope, accountdetail) {
+  .controller('AccountdetailCtrl', function ($scope, accountdetail, $mdDialog) {
 
 
     accountdetail.getTransactions().then(function success(transactions){
       $scope.transactions = transactions;
     });
 
+
+    $scope.transactionDialog = undefined;
+    $scope.openTransaction = _.debounce(openTransaction, 20, true);
+
+
+    return;
+
+
+    /**
+     *
+     * @param transactionId
+     */
+    function openTransaction(transaction){
+      $scope.transactionDialog = $mdDialog.alert({
+        title: 'Transaction ' + transaction.id,
+        content: transaction.info,
+        ok: 'Close'
+      });
+      $mdDialog
+        .show( $scope.transactionDialog )
+        .finally(function() {
+          $scope.transactionDialog = undefined;
+        });
+    }
 
 
   });
--- client/app/accountdetail/accountdetail.service.js
+++ client/app/accountdetail/accountdetail.service.js
@@ -16,31 +16,38 @@ angular.module('trusteesApp')
       return $q.when([
         {
           id: 1,
-          title: 'Transaction 01'
+          title: 'Transaction 01',
+          info: 'Transaction info'
         },
         {
           id: 2,
-          title: 'Transaction 02'
+          title: 'Transaction 02',
+          info: 'Transaction info'
         },
         {
           id: 3,
-          title: 'Transaction 03'
+          title: 'Transaction 03',
+          info: 'Transaction info'
         },
         {
           id: 4,
-          title: 'Transaction 04'
+          title: 'Transaction 04',
+          info: 'Transaction info'
         },
         {
           id: 5,
-          title: 'Transaction 05'
+          title: 'Transaction 05',
+          info: 'Transaction info'
         },
         {
           id: 6,
-          title: 'Transaction 06'
+          title: 'Transaction 06',
+          info: 'Transaction info'
         },
         {
           id: 7,
-          title: 'Transaction 07'
+          title: 'Transaction 07',
+          info: 'Transaction info'
         }
       ]);
     }
